2c0b5efdb8191c1adbd61bed938c3ce8a78ea0ca
hoang190116 <hoangnbhbhgf190116@fpt.edu.vn>
2021-06-02 15:00:58 +0700

    them index, rightcolumn và folder img

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+include('Header-bar-login.php');
+?>
+<div class="row">
+	<div class="leftcolumn">
+			<div class="card card-left">
+			<a href="" class="product">
+				<div class="title">
+				<h2 class="product-title">AA-HD1</h2>
+				</div>
+				<div class="product-img">
+					<img src="img/book1.jpg">
+				</div>
+				<p class="product-price">
+					200$
+				</p>
+			</a>
+			</div>
+
+			<div class="card card-left">
+			<a href="" class="product">
+				<div class="title">
+				<h2 class="product-title">BB-HE2</h2>
+				</div>
+				<div class="product-img">
+					<img src="img/book2.jpg">
+				</div>
+				<p class="product-price">
+					500$
+				</p>
+			</a>
+			</div>
+
+			<div class="card card-left">
+			<a href="" class="product">
+				<div class="title">
+				<h2 class="product-title">AD-HGG4</h2>
+				</div>
+				<div class="product-img">
+					<img src="img/book3.jpg">
+				</div>
+				<p class="product-price">
+					300$
+				</p>
+			</a>
+			</div>
+
+	</div>
+	<?php include('rightcolumn.php'); ?>
+
+</div>
+	<?php include('footer.php'); ?>
